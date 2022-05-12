9e70c5826c884af93f222f599955920142dddf9e
dryisf <daryl.issoufi@gmail.com>
2022-05-12 11:31:38 +0200

    wip: flex div creation

---
+++ src/app/flex-div/flex-div.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flex-div',
+  templateUrl: './flex-div.component.html',
+  styleUrls: ['./flex-div.component.scss']
+})
+export class FlexDivComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
